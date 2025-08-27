09790ef24455c93f70685cd069572eb37756e71c
Felino Malicdem <felinomalicdem22@gmail.com>
2025-08-27 14:32:11 +0800

    27-08-2025

--- Java Mastery/Nested Loops/src/Main.java
+++ Java Mastery/Nested Loops/src/Main.java
@@ -1,2 +1,29 @@
-public class Main {
-}
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        //nested loop = a loop inside another loop; used often with matrices or DS&A
+
+        Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int columns;
+        char symbol;
+
+        System.out.println("Enter the # of rows: ");
+        rows = scanner.nextInt();
+
+        System.out.println("Enter the # of columns: ");
+        columns = scanner.nextInt();
+
+        System.out.println("Enter the symbol to use: ");
+        symbol = scanner.next().charAt(0);
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                System.out.print(symbol);
+                }
+            System.out.println();
+            }
+        }
+    }
